0796fab5d5c348b803dd373998b585e0125a87d2
canele <canele.yabuuchi@gmail.com>
2024-02-13 19:04:44 +0900

    02/13 19:00 テスト用に、init.sqlにてDB書込み有 & app.pyにトップ画面表示用の関数有（いずれも本番では削除必要） butch

---
+++ ChatApp/app.py
@@ -0,0 +1,153 @@
+from flask import Flask, request, session, render_template, redirect, flash
+import hashlib
+import uuid
+from datetime import datetime, timedelta, date
+from models import dbConnect
+import matplotlib.pyplot as plt
+import io
+import base64
+
+app = Flask(__name__)
+app.secret_key = uuid.uuid4().hex
+app.permanent_session_lifetime = timedelta(days=7)
+
+# トップ画面の表示
+@app.route('/')
+def top():
+    return render_template('top.html')
+
+# ログイン画面の表示
+@app.route('/login')
+def show_login():
+    is_instructor = request.args.get('is_instructor')
+    return render_template('login/user_login.html', is_instructor=is_instructor)
+
+# ログイン処理
+@app.route('/login', methods=['POST'])
+def login():
+    mail = request.form.get('mail')
+    password = request.form.get('password')
+    
+    if mail == '' and password == '':
+        flash('メールアドレスとパスワードを入力してください')
+    elif mail == '':
+        flash('メールアドレスを入力してください')
+    elif password == '':
+        flash('パスワードを入力してください')
+    else:
+        user = dbConnect.getUserByMail(mail)
+        if user is None:
+            flash('このメールアドレスは登録されていません')
+        else:
+            hash_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+            if hash_password != user['password']:
+                flash('パスワードが違います')
+            else:
+                session['uid'] = user['u_id']
+                return redirect('/index')
+    return redirect('/login')
+
+# ログアウト処理
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
+
+# メニュー画面の表示
+@app.route('/index')
+def menu():
+    u_id = session.get('uid')
+    if u_id is None:
+        return redirect('/login')
+    else:
+        rooms = dbConnect.getRoomAll(u_id)
+        user = dbConnect.getUserById(u_id)
+        return render_template('menu/index.html', is_instructor=user['is_instructor'], rooms=rooms)
+
+# インストラクター一覧画面
+@app.route('/instructors')
+def instructors():
+    u_id = session.get('uid')
+    if u_id is None:
+        return redirect('/login')
+    else:
+        instructors = dbConnect.getInstructors()
+        return render_template('menu/supporter.html', instructors=instructors)
+
+# チャットルーム作成画面の表示
+@app.route('/add-chatroom')
+def show_add_chatroom():
+    u_id = session.get('uid')
+    if u_id is None:
+        return redirect('/login')
+    else:
+        instructor_name = request.args.get('instructor_name')
+        return render_template('menu/add_chatroom.html', instructor_name=instructor_name)
+
+# チャットルームの作成処理
+@app.route('/add-chatroom', methods=['POST'])
+def add_chatroom():
+    u_id = session.get('uid')
+    if u_id is None:
+        return redirect('/login')
+    else:
+        instructor_name = request.form.get('instructor_name')
+        room_name = request.form.get('room_name')
+        instructor = dbConnect.getUserByName(instructor_name)
+        instructor_id = instructor['u_id']
+        db_room = dbConnect.getRoomByIDs(u_id, instructor_id)
+        if db_room is None:
+            dbConnect.addChatRoom(room_name, u_id, instructor_id)
+            return redirect('/index')
+        else:
+            flash('すでにチャットルームが作成されています')
+            return redirect('/instructors')
+
+# 体重画面の表示
+@app.route('/weight-page')
+def weight_page():
+    u_id = session.get('uid')
+    if u_id is None:
+        return redirect('/login')
+    else:
+        record_room_id = 1
+        span = 'week'
+        records = dbConnect.getRecordAll(record_room_id)
+        room = dbConnect.getRecordRoomById(record_room_id)
+        unit = room['unit']
+        values = [float(x['value']) for x in records]
+        dates = [y['created_at'].date() for y in records]
+        if span == 'week':
+            indicate = 7
+        else:
+            indicate = 30
+            
+        # day_of_week = dates[-1].weekday()
+        indicate_values = values[-indicate:]
+        indicate_dates = [a.strftime('%Y-%m-%d') for a in dates[-indicate:]]
+        graph_dates = [z[-5:] for z in indicate_dates]
+        plt.plot(graph_dates, indicate_values)
+        
+        # グラフを画像として保存
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+        
+        # 画像をBase64にエンコード
+        graph = base64.b64encode(buffer.read()).decode('utf-8')
+        plt.close()
+            
+        return render_template('menu/weight_page.html', graph=graph, indicate=indicate, indicate_values=indicate_values, indicate_dates=indicate_dates, unit=unit)
+
+# 404エラーの画面遷移
+@app.errorhandler(404)
+def error404(error):
+    return render_template('error/404.html'),404
+
+# 500エラーの画面遷移
+@app.errorhandler(500)
+def error404(error):
+    return render_template('error/500.html'),500
+
+if __name__ =='__main__':
+    app.run(host="0.0.0.0", debug=True)
---
+++ ChatApp/models.py
@@ -0,0 +1,296 @@
+import pymysql
+from flask import abort
+from util.DB import DB
+
+
+class dbConnect:
+    # ユーザー情報の追加
+    def createUser(u_id, user_name, mail, password, is_instructor, latest_weight, height, goal, introduction, address, icon_path):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "INSERT INTO users (u_id, user_name, mail, password, is_instructor, latest_weight, height, goal, introduction, address, icon_path) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+            cur.execute(sql, (u_id, user_name, mail, password, is_instructor, latest_weight, height, goal, introduction, address, icon_path))
+            conn.commit()
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+            conn.close()
+    
+    # メールアドレスを指定してユーザー情報の取得
+    def getUserByMail(mail):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM users WHERE mail=%s;"
+            cur.execute(sql, (mail))
+            user = cur.fetchone()
+            return user
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+            conn.close()
+    
+    # ユーザー名を指定してユーザー情報の取得
+    def getUserByName(user_name):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM users WHERE user_name=%s;"
+            cur.execute(sql, (user_name))
+            user = cur.fetchone()
+            return user
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+            conn.close()
+    
+    # ユーザーIDを指定してユーザー情報の取得
+    def getUserById(u_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM users WHERE u_id=%s;"
+            cur.execute(sql, (u_id))
+            user = cur.fetchone()
+            return user
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+            conn.close()
+    
+    # すべてのインストラクター情報を取得
+    def getInstructors():
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM users WHERE is_instructor=1;"
+            cur.execute(sql)
+            instructors = cur.fetchall()
+            return instructors
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+            conn.close()
+    
+    # 記録ルームの追加
+    def addRecordRoom(record_name, u_id, unit, is_public, remind, remind_time):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "INSERT INTO record_setting (record_name, u_id, unit, is_public, remind, remind_time) VALUES (%s, %s, %s, %s, %s, %s);"
+            cur.execute(sql, (record_name, u_id, unit, is_public, remind, remind_time))
+            conn.commit()
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+            conn.close()
+    
+    # 記録ルームIDを指定してルーム情報を取得
+    def getRecordRoomById(record_room_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM record_setting WHERE record_room_id=%s;"
+            cur.execute(sql, (record_room_id))
+            record_room = cur.fetchone()
+            return record_room
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+            conn.close()
+    
+    # ユーザーIDを指定して記録ルーム情報をすべて取得
+    def getRecordRoomAll(u_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM record_setting WHERE u_id=%s;"
+            cur.execute(sql, (u_id))
+            record_rooms = cur.fetchall()
+            return record_rooms
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+            conn.close()
+    
+    # 記録の追加
+    def addRecord(record_room_id, value, created_at):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "INSERT INTO records (record_room_id, value, created_at) VALUES (%s, %s, %s);"
+            cur.execute(sql, (record_room_id, value, created_at))
+            conn.commit()
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+            conn.close()
+    
+    # 記録ルームIDを指定して該当ルームの持つ記録を取得
+    def getRecordAll(record_room_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM records WHERE record_room_id=%s;"
+            cur.execute(sql, (record_room_id))
+            records = cur.fetchall()
+            return records
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+            conn.close()
+    
+    
+    
+    #チャットルームの追加
+    def addChatRoom(room_name, created_u_id, invited_u_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "INSERT INTO rooms (room_name,created_u_id,invited_u_id) VALUES(%s, %s, %s);"
+            cur.execute(sql,(room_name, created_u_id, invited_u_id))
+            conn.commit()
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+            conn.close()
+    
+    #チャットルームIDを指定してルーム情報を取得
+    def getRoomByID(room_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM rooms WHERE room_id =%s;"
+            cur.execute(sql,(room_id))
+            room = cur.fetchone()
+            return room
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+    
+    #ユーザーIDを指定してチャットルーム情報をすべて取得
+    def getRoomAll(u_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM rooms WHERE created_u_id = %s OR invited_u_id = %s;"
+            cur.execute(sql,(u_id, u_id))
+            rooms = cur.fetchall()
+            return rooms
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+            conn.close()
+    # def getRoomAll(created_u_id):
+    #     try:
+    #         conn = DB.getConnection()
+    #         cur = conn.cursor()
+    #         sql = "SELECT * FROM rooms WHERE created_u_id =%s;"
+    #         cur.execute(sql,(created_u_id))
+    #         rooms = cur.fetchall()
+    #         return rooms
+    #     except Exception as e:
+    #         print(str(e) + 'が発生しています')
+    #         abort(500)
+    #     finally:
+    #         cur.close()
+    
+    #created_u_idとinvited_u_idを指定してチャットルーム情報を取得
+    def getRoomByIDs(created_u_id, invited_u_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM rooms WHERE created_u_id = %s AND invited_u_id = %s;"
+            cur.execute(sql,(created_u_id, invited_u_id))
+            room = cur.fetchone()
+            return room
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+            conn.close()
+
+    #チャットルームIDを指定してルーム情報を削除
+    def deleteChatRoom(room_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "DELETE * FROM rooms WHERE room_id =%s;"
+            cur.execute(sql,(room_id))
+            conn.commit()
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+
+    #メッセージの追加
+    def aaddMessage(u_id, room_id, message, created_at):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "INSERT INTO messages(u_id, room_id, message, created_at) VALUES(%s, %s, %s,%s);"
+            cur.execute(sql,(u_id, room_id, message, created_at))
+            conn.commit()
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()
+
+    #チャットルームIDを指定して該当チャンネルの持つメッセージを取得
+    def getMessageAll(room_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM messages WHERE room_id =%s;"
+            cur.execute(sql,(room_id))
+            massages = cur.fetchall()
+            return massages
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()  
+
+    #メッセージIDを指定してメッセージ情報を削除
+    def deleteMessage(message_id):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "DELETE * FROM massages WHERE massage_id =%s;"
+            cur.execute(sql,(massage_id))
+            conn.commit()
+        except Exception as e:
+            print(str(e) + 'が発生しています')
+            abort(500)
+        finally:
+            cur.close()

6b02a92d198edfedf8b058fb5cd2953ab0f0db3d
akimizu21 <10akihiro15@gmail.com>
2024-02-22 01:03:47 +0900

    2/22 1:03 フロント最新版に変更　あき

--- ChatApp/static/js/chat2.js
+++ ChatApp/static/js/chat2.js
@@ -1,11 +1,3 @@
-const userInfoBar = document.getElementById('user-info-bar');
-const userInfo = document.getElementById('user-info');
-
-// ユーザー情報バーがクリックされたときに、ユーザー情報を切り替える
-userInfoBar.addEventListener('click', () => {
-    userInfo.classList.toggle('show');
-});
-
 document.getElementById("chatroom-form").addEventListener("submit", function(event) {
     // ダイアログメッセージを表示
     alert("チャットルームが作成されました！");
